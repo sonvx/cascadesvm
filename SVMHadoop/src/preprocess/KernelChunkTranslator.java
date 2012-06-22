09a8fbad1cd9d3bd4d2b006b795bcedd00966ded
roadjiang <roadjiang@e0add914-414b-47d3-dadf-3c017ad33da8>
2012-06-22 19:59:29 +0000

    Add translate dir function in PredictionTranslator Add minus translate in KenrlChunkTranslator

--- SVMHadoop/src/preprocess/KernelChunkTranslator.java
+++ SVMHadoop/src/preprocess/KernelChunkTranslator.java
@@ -34,9 +34,34 @@ public class KernelChunkTranslator {
 	}
 	
 	
+	
+	/**
+	 * 
+	 * translate the binary kernel file into a plain text file.(All float)
+	 * @param in the input 
+	 * @param outdir the output file i.e. the text file 
+	 * @param dim the dimension of kernel (233667)
+	 * @throws IOException
+	 */
+	public static void translateMinus(File in, File outfile, int dim) throws IOException {
+		BufferedWriter bw = new BufferedWriter(new FileWriter(outfile),1024*8*16);
+		DataInputStream br = new DataInputStream(new FileInputStream(in));
+		while(br.available() > 0) {
+			for(int i = 0 ; i < dim -1; i ++) {
+				float kernel = br.readFloat() * -1.0f;
+				bw.write(kernel + " ");
+			}
+			
+			bw.write((-1.0f*br.readFloat()) + "\r\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+	
+	
 	public static void main(String[] args) throws Exception {
 		
-		translate(new File(args[0]), new File(args[1]), Integer.parseInt(args[2]));
+		translateMinus(new File(args[0]), new File(args[1]), Integer.parseInt(args[2]));
 
 	}
 
