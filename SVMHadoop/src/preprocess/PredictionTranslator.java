e0ffed1a76b6d2bbf691ee06cac4de8307d09e4a
roadjiang <roadjiang@e0add914-414b-47d3-dadf-3c017ad33da8>
2012-07-22 22:27:17 +0000

    add  a base id number while translating

--- SVMHadoop/src/preprocess/PredictionTranslator.java
+++ SVMHadoop/src/preprocess/PredictionTranslator.java
@@ -9,12 +9,16 @@ import java.io.IOException;
 
 public class PredictionTranslator {
 	
-	public static void translateDir(File in, File outdir, int dim) throws IOException {
+
+	
+	public static void translateDir(File in, File outdir, int dim, int baseindex) throws IOException {
 		File[] subfiles = in.listFiles();
 		for(int i = 0 ; i < subfiles.length ; i++) {
-			translate(subfiles[i], outdir, dim);
+			translate(subfiles[i], outdir, dim, baseindex);
 		}
 	}
+	
+
 	
 	/**
 	 * 
@@ -24,16 +28,28 @@ public class PredictionTranslator {
 	 * @param dim the dimension of prediction (346)
 	 * @throws IOException
 	 */
-	public static void translate(File in, File outdir, int dim) throws IOException {
+	public static void translate(File in, File outdir, int dim, int baseindex) throws IOException {
 		BufferedWriter bw = new BufferedWriter(new FileWriter(new File(outdir,in.getName()+".txt")),1024*8*16);
 		DataInputStream br = new DataInputStream(new FileInputStream(in));
 		while(br.available() > 0) {
-			for(int i = 0 ; i < dim+1 ; i ++) {
+			double linnum = br.readDouble();
+			int id = (int)linnum + baseindex;
+			/*{
+				int p = Integer.parseInt(in.getName().substring(in.getName().indexOf("-p")+2));
+				id = id + p * 50;
+						
+			}*/
+			bw.write(id + " ");
+			for(int i = 0 ; i < (dim-1) ; i ++) {
 				double prediction = br.readDouble();
 				bw.write(prediction + " ");
 			}
+			
+			double prediction = br.readDouble();
+			bw.write(""+prediction);
 			bw.write("\r\n");
 		}
+		
 		bw.flush();
 		bw.close();
 	}
@@ -41,7 +57,7 @@ public class PredictionTranslator {
 	
 	public static void main(String[] args) throws Exception {
 		
-		translateDir(new File(args[0]), new File(args[1]), Integer.parseInt(args[2]));
+		translateDir(new File(args[0]), new File(args[1]), Integer.parseInt(args[2]), Integer.parseInt(args[3]));
 
 	}
 
