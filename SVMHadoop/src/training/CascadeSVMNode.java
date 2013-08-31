cd413fae7cc6675453fd0f28833d63e450753513
lightxuzju@gmail.com <lightxuzju@gmail.com@e0add914-414b-47d3-dadf-3c017ad33da8>
2013-08-31 00:18:43 +0000

    Finish: Node

--- SVMHadoop/src/training/CascadeSVMNode.java
+++ SVMHadoop/src/training/CascadeSVMNode.java
@@ -1,15 +1,12 @@
 package training;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.MapReduceBase;
@@ -24,70 +21,66 @@ import libsvm.svm_model;
 import libsvm.svm_node;
 import libsvm.svm_parameter;
 import libsvm.svm_problem;
+import local.KernelProjector;
 
 public class CascadeSVMNode extends MapReduceBase 
 		implements Mapper<IntWritable, Text, IntWritable, Text>,
 		Reducer<IntWritable, Text, IntWritable, Text> {
+	private static boolean verbose;
 	private static Logger logger;
+	private static double[] tune_parameter; // = {0.125d};
+	private static int randomSeed;
+	static {
+		verbose = true;
+		logger = Logger.getLogger(CascadeSVMNode.class);
+		tune_parameter = new double[11];
+		for (int i = -10; i <= 10; i += 2)
+			tune_parameter[i] = Math.pow(2, i);
+		randomSeed = 0;
+	}
 
 	@Override
 	public void map(IntWritable key, Text value,
 			OutputCollector<IntWritable, Text> output, Reporter reporter)
 			throws IOException {
-		
+		CascadeSVMNodeParameter parameter = null;
+		try {
+			parameter = new CascadeSVMNodeParameter(value.toString());
+		} catch (CascadeSVMParameterFormatError e) {
+			return ;
+		}
+		svm.rand.setSeed(randomSeed);
+		ArrayList<Integer> idList = CascadeSVMIOHelper.readIdListHadoop(parameter.idlistPath);
+		double[] labels = CascadeSVMIOHelper.readLabelHadoop(parameter.labelPath, idList);
+		float[][] kernel = projectKernelHadoop(parameter, idList);						        // project kernel
+		svm_problem problem = convert2SVMProblem(idList, kernel, labels);									// create svm problem
+		kernel = null; // GC
+		svm_parameter param = crossValidation(problem, parameter.nFold, randomSeed);						// cross validation
+		svm_model model = svm.svm_train(problem, param); 													// train
+		CascadeSVMIOHelper.writeModelHadoop(parameter.modelPath, model);
+		CascadeSVMIOHelper.writeSVIdListHadoop(parameter.SVPath, model, idList);
+		double LD = computeLD(model, problem);
+		CascadeSVMIOHelper.writeLDHadoop(parameter.LDPath, LD);
 	}
 	
 	@Override
 	public void reduce(IntWritable key, Iterator<Text> value,
 			OutputCollector<IntWritable, Text> output, Reporter reporter)
 			throws IOException {
-		// TODO Auto-generated method stub
-		
+		// Nothing to do.
 	}
 	
+	public static float[][] projectKernelHadoop(CascadeSVMNodeParameter parameter, ArrayList<Integer> idList) throws IOException {
+		KernelProjector projector = new KernelProjector();
+		Configuration conf = new Configuration();
+		FileSystem fs = FileSystem.get(conf);
+		float[][] kernel = projector.projectHadoop(fs, parameter.kernelPath, idList, parameter.dimension);
+		return kernel;
+	} 
 	
-	
-	private static double[] tune_parameter; // = {0.125d};
-	private static int randomSeed = 0;
-	static {
-		logger = Logger.getLogger(CascadeSVMNode.class);
-		tune_parameter = new double[11];
-		for (int i = -10; i <= 10; i += 2)
-			tune_parameter[i] = Math.pow(2, i);
-	}
-	
-	public svm_problem problem;
-	public svm_model model;
-	public ArrayList<Integer> idList;
-	public double LD;
-	
-	private boolean verbose = true;
-	
-	public CascadeSVMNode(
-			String kerneLDath, 
-			String labeLDath, 
-			String idListPath, 
-			int nrFold, 
-			String modelOutputPath,
-			String SVIdOutputPath)
-					throws NumberFormatException, FileNotFoundException, IOException {
-		if (verbose) logger.info("[Begin]CascadeSVMNode");
-		svm.rand.setSeed(randomSeed);
-		idList = readIdList(new BufferedReader(new FileReader(idListPath))); // read id list
-		double[] labels = readLabel(idList, new BufferedReader(new FileReader(labeLDath)));		// read label
-		float[][] kernel = projectKernel(new BufferedReader(new FileReader(kerneLDath)), idList);								// project kernel
-		problem = convert2SVMProblem(idList, kernel, labels);									// create svm problem
-		kernel = null; // GC
-		svm_parameter param = crossValidation(problem, nrFold, randomSeed);						// cross validation
-		model = svm.svm_train(problem, param); 													// train
-		svm.svm_save_model(modelOutputPath, model);												// output model
-		writeSVId(new BufferedWriter(new FileWriter(SVIdOutputPath)));
-		if (verbose) logger.info("[End]CascadeSVMNode");
-	}
-	
-	public double computeLD() {
+	public static double computeLD(svm_model model, svm_problem problem) {
 		if (verbose) logger.info("[Begin]Compute LD");
-		LD = 0;
+		double LD = 0;
 		int[] sv_indices = new int[model.l];
 		for (int i = 0; i < model.l; i++) {
 			sv_indices[i] = (int)model.SV[i][0].value - 1; // sv id starts from 1
@@ -100,42 +93,8 @@ public class CascadeSVMNode extends MapReduceBase
 		if (verbose) logger.info("[End]Compute LD");
 		return LD;
 	}
-	
-	public void writeLD(String LDOutputPath) throws IOException {
-		BufferedWriter writer = new BufferedWriter(new FileWriter(LDOutputPath));
-		double LD = computeLD();
-		writer.write(Double.toString(LD));
-		writer.close();
-	}
-	
-	private ArrayList<Integer> readIdList(BufferedReader idListFile)
-			throws NumberFormatException, IOException {
-		if (verbose) logger.info("[Begin]readIdList");
-		ArrayList<Integer> idList = new ArrayList<Integer>();
-		String line;
-		while ((line = idListFile.readLine()) != null) {
-			int id = Integer.parseInt(line);
-			idList.add(new Integer(id));
-		}
-		idListFile.close();
-		// Sort the id list
-		Collections.sort(idList);
-		if (verbose) logger.info("[End]readIdList");
-		return idList;
-	}
 
-	private svm_problem convert2SVMProblem(ArrayList<Integer> idList, float[][] kernel, double[] labels) {
-		/*  New training instance for xi:
-			<label> 0:i 1:K(xi,x1) ... L:K(xi,xL) 
-			New testing instance for any x:
-			<label> 0:? 1:K(x,x1) ... L:K(x,xL) 
-			That is, in the training file the first column must be the "ID" of
-			xi. In testing, ? can be any value.
-			
-			Shicheng: So... we can use id here?
-			Shicheng: No, we can't. We should use sequential id start from 1
-			So, the id of support vector should be convert into the original id when print them.
-		 */
+	public static svm_problem convert2SVMProblem(ArrayList<Integer> idList, float[][] kernel, double[] labels) {
 		if (verbose) logger.info("[Start]convert2SVMProblem");
 		svm_problem prob = new svm_problem();
 		
@@ -159,7 +118,7 @@ public class CascadeSVMNode extends MapReduceBase
 		return prob;
 	}
 	
-	private svm_parameter crossValidation(svm_problem problem, int nrFold, double randomSeed) {
+	public static svm_parameter crossValidation(svm_problem problem, int nrFold, double randomSeed) {
 		 if (verbose) logger.info("[Start]crossValidation");
 		 double[] target = new double[problem.y.length];
 		 @SuppressWarnings("unchecked")
@@ -198,16 +157,6 @@ public class CascadeSVMNode extends MapReduceBase
 		 return bestParameter;
 	}
 	
-	private void writeSVId(BufferedWriter SVIdOutputFile) throws IOException {
-		if (verbose) logger.info("[Start]writeSVId");
-		for (int i = 0; i < model.l; i++) {
-			SVIdOutputFile.write(Integer.toString(idList.get((int)model.SV[i][0].value - 1)));
-			SVIdOutputFile.newLine();
-		}
-		SVIdOutputFile.close();
-		if (verbose) logger.info("[End]writeSVId");
-	}
-	
 	/**
 	 * Format of probability
 	 * 1.0 0.0
@@ -217,7 +166,7 @@ public class CascadeSVMNode extends MapReduceBase
 	 * @param probabilities
 	 * @return
 	 */
-	private double computeAveragePrecision(double[] label, ArrayList<Double>[] probabilities) {
+	public static double computeAveragePrecision(double[] label, ArrayList<Double>[] probabilities) {
 		if (verbose) logger.info("[Start]computeAveragePrecision");
 		int labelIndex = 0;
 		if (probabilities[0].get(1) == 1) {
@@ -242,7 +191,7 @@ public class CascadeSVMNode extends MapReduceBase
 		return map;
 	}
 	
-	private void changeProblem(svm_problem prob, double scale) {
+	public static void changeProblem(svm_problem prob, double scale) {
 		if (verbose) logger.info("[Start]changeProblem");
 		for (int i = 0; i < prob.x.length; i++) {
 			for (int j = 1; j < prob.x[i].length; j++) {
@@ -252,7 +201,7 @@ public class CascadeSVMNode extends MapReduceBase
 		if (verbose) logger.info("[End]changeProblem");
 	}
 	
-	private svm_parameter getDefaultParams() {
+	public static svm_parameter getDefaultParams() {
 		svm_parameter param = new svm_parameter();
 		param.svm_type = svm_parameter.C_SVC;
 		param.kernel_type = svm_parameter.PRECOMPUTED;
@@ -272,7 +221,7 @@ public class CascadeSVMNode extends MapReduceBase
 		return param;
 	}
 
-	private static double[] calculateAP(ArrayList<CrossValidationItem> crossValidationItems) {
+	public static double[] calculateAP(ArrayList<CrossValidationItem> crossValidationItems) {
 		double[] result = new double[2];
 		result[0] = 0;
 		result[1] = -1;
@@ -298,59 +247,115 @@ public class CascadeSVMNode extends MapReduceBase
 	}
 	
 	// TEST
-	/**
-	 * This is the self-debug version.
-	 * @param kerneLDath
-	 * @param idListPath
-	 * @return
-	 * @throws IOException 
-	 * @throws NumberFormatException 
-	 */
-	private float[][] projectKernel(BufferedReader kernelFile, ArrayList<Integer> idList) throws NumberFormatException, IOException {
-		if (verbose) logger.info("[Begin]projectKernel");
-		int n = idList.size();
-		float[][] kernel = new float[n][n];
-		int row = 0;
-		int i = 0;
-		String line;
-		while ((line = kernelFile.readLine()) != null) {
-			if (idList.get(i).intValue() - 1 == row) {
-				String[] valueArray = line.split(" ");
-				for (int j = 0; j < n; j++)
-					try {
-						kernel[i][j] = Float.parseFloat(valueArray[idList.get(j).intValue() - 1]);
-					} catch (ArrayIndexOutOfBoundsException e) {
-						logger.info("[Error]array index out of bound");
-						logger.info(Integer.toString(idList.get(j).intValue() - 1));
-					}
-				i++;
-				if (i == n)
-					break;
-			}
-			row++;
-		}
-		kernelFile.close();
-		if (verbose) logger.info("[End]projectKernel");
-		return kernel;
-	}
+
+//	public CascadeSVMNode(
+//			String kerneLDath, 
+//			String labeLDath, 
+//			String idListPath, 
+//			int nrFold, 
+//			String modelOutputPath,
+//			String SVIdOutputPath)
+//					throws NumberFormatException, FileNotFoundException, IOException {
+//		if (verbose) logger.info("[Begin]CascadeSVMNode");
+//		svm.rand.setSeed(randomSeed);
+//		idList = readIdList(new BufferedReader(new FileReader(idListPath))); // read id list
+//		double[] labels = readLabel(idList, new BufferedReader(new FileReader(labeLDath)));		// read label
+//		float[][] kernel = projectKernel(new BufferedReader(new FileReader(kerneLDath)), idList);								// project kernel
+//		problem = convert2SVMProblem(idList, kernel, labels);									// create svm problem
+//		kernel = null; // GC
+//		svm_parameter param = crossValidation(problem, nrFold, randomSeed);						// cross validation
+//		model = svm.svm_train(problem, param); 													// train
+//		svm.svm_save_model(modelOutputPath, model);												// output model
+//		writeSVId(new BufferedWriter(new FileWriter(SVIdOutputPath)));
+//		if (verbose) logger.info("[End]CascadeSVMNode");
+//	}
 	
-	public static void main(String[] args) {
-		String kerneLDath = "/home/lightxu/Work/cascadesvm_note/train_single/kernel.txt";
-		String labeLDath = "/home/lightxu/Work/cascadesvm_note/train_single/label.txt"; 
-		String idListPath = "/home/lightxu/Work/cascadesvm_note/train_single/idlist.txt";
-		int nrFold = 5;
-		String modelOutputPath = "/home/lightxu/Work/cascadesvm_note/train_single/model.txt";
-		String SVIdOutputPath = "/home/lightxu/Work/cascadesvm_note/train_single/svid.txt";
-		try {
-			CascadeSVMNode svm = new CascadeSVMNode(kerneLDath, labeLDath, idListPath, nrFold, modelOutputPath, SVIdOutputPath);
-			double LD = svm.computeLD();
-			System.out.println(LD);
-		} catch (NumberFormatException e) {
-			e.printStackTrace();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+//	public void writeLD(String LDOutputPath) throws IOException {
+//		BufferedWriter writer = new BufferedWriter(new FileWriter(LDOutputPath));
+//		double LD = computeLD();
+//		writer.write(Double.toString(LD));
+//		writer.close();
+//	}
+//	
+//	private ArrayList<Integer> readIdList(BufferedReader idListFile)
+//			throws NumberFormatException, IOException {
+//		if (verbose) logger.info("[Begin]readIdList");
+//		ArrayList<Integer> idList = new ArrayList<Integer>();
+//		String line;
+//		while ((line = idListFile.readLine()) != null) {
+//			int id = Integer.parseInt(line);
+//			idList.add(new Integer(id));
+//		}
+//		idListFile.close();
+//		// Sort the id list
+//		Collections.sort(idList);
+//		if (verbose) logger.info("[End]readIdList");
+//		return idList;
+//	}
+//	
+//	private void writeSVId(BufferedWriter SVIdOutputFile) throws IOException {
+//		if (verbose) logger.info("[Start]writeSVId");
+//		for (int i = 0; i < model.l; i++) {
+//			SVIdOutputFile.write(Integer.toString(idList.get((int)model.SV[i][0].value - 1)));
+//			SVIdOutputFile.newLine();
+//		}
+//		SVIdOutputFile.close();
+//		if (verbose) logger.info("[End]writeSVId");
+//	}
+//	
+//	/**
+//	 * This is the self-debug version.
+//	 * @param kerneLDath
+//	 * @param idListPath
+//	 * @return
+//	 * @throws IOException 
+//	 * @throws NumberFormatException 
+//	 */
+//	private float[][] projectKernel(BufferedReader kernelFile, ArrayList<Integer> idList) throws NumberFormatException, IOException {
+//		if (verbose) logger.info("[Begin]projectKernel");
+//		int n = idList.size();
+//		float[][] kernel = new float[n][n];
+//		int row = 0;
+//		int i = 0;
+//		String line;
+//		while ((line = kernelFile.readLine()) != null) {
+//			if (idList.get(i).intValue() - 1 == row) {
+//				String[] valueArray = line.split(" ");
+//				for (int j = 0; j < n; j++)
+//					try {
+//						kernel[i][j] = Float.parseFloat(valueArray[idList.get(j).intValue() - 1]);
+//					} catch (ArrayIndexOutOfBoundsException e) {
+//						logger.info("[Error]array index out of bound");
+//						logger.info(Integer.toString(idList.get(j).intValue() - 1));
+//					}
+//				i++;
+//				if (i == n)
+//					break;
+//			}
+//			row++;
+//		}
+//		kernelFile.close();
+//		if (verbose) logger.info("[End]projectKernel");
+//		return kernel;
+//	}
+//	
+//	public static void main(String[] args) {
+//		String kerneLDath = "/home/lightxu/Work/cascadesvm_note/train_single/kernel.txt";
+//		String labeLDath = "/home/lightxu/Work/cascadesvm_note/train_single/label.txt"; 
+//		String idListPath = "/home/lightxu/Work/cascadesvm_note/train_single/idlist.txt";
+//		int nrFold = 5;
+//		String modelOutputPath = "/home/lightxu/Work/cascadesvm_note/train_single/model.txt";
+//		String SVIdOutputPath = "/home/lightxu/Work/cascadesvm_note/train_single/svid.txt";
+//		try {
+//			CascadeSVMNode svm = new CascadeSVMNode(kerneLDath, labeLDath, idListPath, nrFold, modelOutputPath, SVIdOutputPath);
+//			double LD = svm.computeLD();
+//			System.out.println(LD);
+//		} catch (NumberFormatException e) {
+//			e.printStackTrace();
+//		} catch (FileNotFoundException e) {
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//	}
 }
