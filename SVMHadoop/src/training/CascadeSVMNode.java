3547ce58589e36bb38ff74c2bfd043a5f5e25836
lightxuzju@gmail.com <lightxuzju@gmail.com@e0add914-414b-47d3-dadf-3c017ad33da8>
2013-09-25 14:15:14 +0000

    remove warning of CascadesSVMNode

--- SVMHadoop/src/training/CascadeSVMNode.java
+++ SVMHadoop/src/training/CascadeSVMNode.java
@@ -2,7 +2,6 @@ package training;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Iterator;
 
 import org.apache.hadoop.conf.Configuration;
